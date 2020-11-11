651b392f5541e75d141808405249e6e43c89f937
Bianca Barria <bianca.barria@beetrack.com>
2020-11-11 16:21:11 -0300

    feature: right configuration enviroments

--- configurations.py
+++ configurations.py
@@ -1,22 +1,20 @@
 import os
 
 class Configurations():
-
     SECRET_KEY = "1234abcd"
     HOST = "localhost:5000"
-
+    
     SHOPIFY_CFG = {
-        'API_KEY' : "9d4b7691f204f97e02489042392fb373",
-        'API_SECRET' : "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",
+        'API_KEY' : os.environ.get('API_KEY_SHOPIFY'),
+        'API_SECRET' : os.environ.get('API_SECRET_SHOPIFY'),
         'APP_HOME' : 'http://' + HOST,
         'CALLBACK_URL' : 'http://' + HOST + '/install',
         'REDIRECT_URI' : 'http://' + HOST + '/connect',
         'SCOPE' : 'write_orders'
     }
-
     DB_CFG = {
-        'DB_NAME' : "beetrack-shopify-db",
-        'DB_USER_NAME' : "bianca",
-        'DB_PASS' : "123456",
-        'DB_HOST' : "localhost"
+        'DB_NAME' : os.environ.get('SHOPIFY_DB_NAME'),
+        'DB_USER_NAME' : os.environ.get('SHOPIFY_DB_USER'),
+        'DB_PASS' : os.environ.get('SHOPIFY_DB_PASS'),
+        'DB_HOST' : os.environ.get('SHOPIFY_DB_HOST')
     }
