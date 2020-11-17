0327bd3e9a06426f3c2e13eba389d90b7b133ea4
Bianca Barria <bianca.barria@beetrack.com>
2020-11-17 15:31:57 -0300

    fix: handler for resources and error message

--- resources/beetrack.py
+++ resources/beetrack.py
@@ -6,7 +6,9 @@ class Beetrack(Resource):
 
     def get(self, account_uuid):
         beetrack_credential_obj = BeetrackCredentialsModel.find_by_uuid(account_uuid)
-        shopify_credential_obj = beetrack_credential_obj.shop.shopify_credentials.pop()
-        if beetrack_credential_obj or shopify_credential_obj:
-            return shopify_credential_obj.serialize()
-        return {'message': 'Shopify credentials were not found for these Beetrack uuid account'}, 404
+        try:
+            shopify_credential_obj = beetrack_credential_obj.shop.shopify_credentials.pop()
+            if beetrack_credential_obj or shopify_credential_obj:
+                return shopify_credential_obj.serialize()
+        except:
+            return {'Message': 'Shopify Credentials were not found for Beetrack UUID Account'}, 404
