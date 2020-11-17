5067347395eb156ab3b11f49f643dfb70bae8119
Bianca Barria <bianca.barria@beetrack.com>
2020-11-17 12:28:03 -0300

    feature: right functions for configuration views

--- views/configuration.py
+++ views/configuration.py
@@ -4,20 +4,25 @@ from api.beetrack import BeetrackApiHandler
 configuration = Blueprint('configuration', __name__)
 
 @configuration.route('/configuration', methods= ['GET', 'POST'])
-def add_api_key():
+def create_api_key():
     if request.method == "GET":
         shop = request.args.get('shop')
         session['shop'] = shop
         return render_template("configuration.html")
-
     elif request.method == 'POST':
         beetrack_api_key = request.form['api_key']
-        verify_beetrack_account = BeetrackApiHandler(beetrack_api_key).verify_apikey()
-        if verify_beetrack_account:
-            session["beetrack_api_key"] = beetrack_api_key
-            return redirect('/install')
-        else:
-            flash("Wrong API KEY, check your configurations.")
-            return render_template('configuration.html')
+        verify_beetrack_key = verify_api_key(beetrack_api_key)
+        return verify_beetrack_key
     else:
          return render_template('error.html')
+
+def verify_api_key(beetrack_api_key):
+    verify_beetrack_account = BeetrackApiHandler(beetrack_api_key).verify_apikey()
+    if verify_beetrack_account:
+        session["beetrack_api_key"] = beetrack_api_key
+        return redirect('/install')
+    else:
+        flash("Wrong API Key: {}, check your Beetrack account configurations.".format(beetrack_api_key))
+        return render_template('configuration.html')
+
+
